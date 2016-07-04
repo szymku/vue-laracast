87488054633a87780c54a9ff69a1ed9ee9ab0de3
Szymon Kulikowski <szymon.kulikowski@mall.cz>
2016-07-04 15:21:36 +0200

    working with template, function in it

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
+Vue.component('tasks', {
+    template: '#tasks-template',
+    props: ['list'],
+    computed: {
+        remaining: function () {
+            return this.list.filter(this.inProgress).length;
+        }
+    },
+    methods: {
+        isCompleted: function (task) {
+            return task.completed;
+        },
+        inProgress: function (task) {
+            return !this.isCompleted(task)
+        },
+        deleteTask:function (task) {
+            this.list.$remove(task);
+        },
+        clearCompleted:function () {
+            this.list = this.list.filter(this.inProgress);
+        }
+    }
+});
+
+
 new Vue({
     el: '#app',
 
     data: {
         tasks: [
-            {body: 'go to work', completed: false},
+            {body: 'go to work', completed: true},
             {body: 'go to home', completed: false},
             {body: 'eat something', completed: false},
-            {body: 'sleep', completed: true}
+            {body: 'sleep', completed: false}
         ]
-    },
-    methods: {
-        toggleLine: function (task) {
-            task.completed = !task.completed
-        }
     }
-
 });
